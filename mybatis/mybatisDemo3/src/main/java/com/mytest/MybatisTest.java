6db1d21178af6be46d298eee201a963341023736
chensongqi <chensongqi@zhongan.com>
2019-12-15 15:03:50 +0800

    spring 整合

--- mybatis/mybatisDemo3/src/main/java/com/mytest/MybatisTest.java
+++ mybatis/mybatisDemo3/src/main/java/com/mytest/MybatisTest.java
@@ -1,49 +1,36 @@
 package com.mytest;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.InputStream;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MybatisTest {
 
-    // 动态代理开发
+    // mybatis spring 整合
     /**
-     * Mapper接口开发需要遵循以下规范：
-     * 1、Mapper.xml文件中的namespace与mapper接口的类路径相同。
-     * 2、Mapper接口方法名和Mapper.xml中定义的每个statement的id相同
-     * 3、Mapper接口方法的输入参数类型和mapper.xml中定义的每个sql 的parameterType的类型相同
-     * 4、Mapper接口方法的输出参数类型和mapper.xml中定义的每个sql的resultType的类型相同
+     * 整合思路
+     * 1、SqlSessionFactory对象应该放到spring容器中作为单例存在。
+     * 2、传统dao的开发方式中，应该从spring容器中获得sqlsession对象。
+     * 3、Mapper代理形式中，应该从spring容器中直接获得mapper的代理对象。
+     * 4、数据库的连接以及数据库连接池事务管理都交给spring容器来完成。
      */
 
-    private SqlSessionFactory sqlSessionFactory;
+    private ApplicationContext context;
 
     @Before
-    public void init() throws Exception {
-        // 创建SqlSessionFactoryBuilder
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        // 加载SqlMapConfig.xml配置文件
-        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
-        // 创建SqlsessionFactory
-        this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+    public void setUp() throws Exception {
+        this.context = new ClassPathXmlApplicationContext("applicationContext.xml");
     }
 
     @Test
     public void testQueryUserById() {
-        // 获取sqlSession，和spring整合后由spring管理
-        SqlSession sqlSession = this.sqlSessionFactory.openSession();
+        // 获取userDao
+        UserMapper userMapper = this.context.getBean(UserMapper.class);
 
-        // 从sqlSession中获取Mapper接口的代理对象
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        // 执行查询方法
         User user = userMapper.queryUserById(1);
         System.out.println(user);
-
-        // 和spring整合后由spring管理
-        sqlSession.close();
     }
+
 }
